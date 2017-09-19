59491e41df189ac0ef13cba09223fdc2042e42ba
Aman Soni <me@amansoni.com>
2017-09-19 08:05:10 +0100

    remove manual logging

--- sdp/model.py
+++ sdp/model.py
@@ -1,97 +1,53 @@
-import argparse
-import logging
+import tempfile
 import sys
-# import random
-import numpy as np
 import gym
 from gym import wrappers
 
 
-""" Q-Learning Agent for experiments with the SDP Framework. Implementation of Q Learning from Watkins, Christopher John Cornish Hellaby. 
-Learning from delayed rewards. Diss. University of Cambridge, 1989.
+""" Model Agent for MountainCar which maximises the velocity by pushing 
+in the direction of movement. The model agent can be optimised
+by not pushing only part way up the left hill.
 
-Link (http://www.cs.rhul.ac.uk/~chrisw/thesis.html)
+State space
+0 position [-1.2, 0.6] goal 0.5
+1 velocity [-0.07, 0.07]
 
+Action space
+0	push left
+1	no push
+2	push right
 """
-class QLearningAgent(object):
-    seed = 0
-    accumulated_reward = 0
-    no_of_velocity_bins = 15
-    no_of_position_bins = 19
-    no_of_actions = 3
-    epsilon = 0.01
-    q = None
-    previous_observation = None
-    previous_action = None
+class ModelAgent(object):
 
-    def __init__(self, env, seed):
+    def __init__(self, env):
         self.env = env
-        self.action_n = env.action_space.n
-        self.observation_space = env.observation_space
-        np.random.seed(seed)
-
-    """
-    0 position -1.2:0.6
-    1 velocity -0.07:0.07
-    """
 
     def act(self, observation, reward, step, done):
-        print("observation {}",observation)
-	action = 2
 	if observation[1] < 0:
 		action = 0
-	elif observation[1] > 0:
+	elif observation[1] >= 0:
 		action = 2
 
         return action
 
-        
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description=None)
-    parser.add_argument('env_id', nargs='?', default='MountainCar-v0', help='Select the environment to run')
-    args = parser.parse_args()
-
-    # Call `undo_logger_setup` if you want to undo Gym's logger setup
-    # and configure things manually. (The default should be fine most
-    # of the time.)
-    gym.undo_logger_setup()
-    logger = logging.getLogger()
-    formatter = logging.Formatter('[%(asctime)s] %(message)s')
-    handler = logging.StreamHandler(sys.stderr)
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-
-    # You can set the level to logging.DEBUG or logging.WARN if you
-    # want to change the amount of output.
-    logger.setLevel(logging.INFO)
-
-    env = gym.make(args.env_id)
-
-    # You provide the directory to write to (can be an existing
-    # directory, including one with existing data -- all monitor files
-    # will be namespaced). You can also dump to a tempdir if you'd
-    # like: tempfile.mkdtemp().
-    outdir = '/tmp/model-agent-results'
+    env = gym.make('MountainCar-v0')
+    outdir = tempfile.mkdtemp()
     env = wrappers.Monitor(env, directory=outdir, force=True)
     env.seed(0)
-    agent = QLearningAgent(env, 42)
+    agent = ModelAgent(env)
 
-    episode_count = 100
+    total_reward = 0
     reward = 0
     done = False
-    total_steps = 100
     ob = env.reset()
-    for step in range(total_steps):
-        while True:
-            action = agent.act(ob, reward, step, done)
-	    print(action)
-            ob, reward, done, _ = env.step(action)
-            reward += reward
-            if done:
-                break
-            # Note there's no env.render() here. But the environment still can open window and
-            # render if asked by env.monitor: it calls env.render('rgb_array') to record video.
-            # Video is not recorded every episode, see capped_cubic_video_schedule for details.
-
-    # Close the env and write monitor result info to disk
+    step = 0
+    while True:
+    	action = agent.act(ob, reward, step, done)
+    	ob, reward, done, _ = env.step(action)
+	step = step + 1
+	total_reward += reward
+    	if done:
+	    print(("Solved in {} steps with a total reward of {}").format(step, total_reward))
+            break
     env.close()
--- sdp/q-learning.py
+++ sdp/q-learning.py
@@ -6,66 +6,49 @@ import gym
 from gym import wrappers
 
 
-""" Q-Learning Agent for experiments with the SDP Framework. Implementation of Q Learning from Watkins, Christopher John Cornish Hellaby. 
-Learning from delayed rewards. Diss. University of Cambridge, 1989.
+""" Q-Learning Agent for experiments with the SDP Framework. Implementation of Q Learning from Watkins, Christopher John Cornish Hellaby. Learning from delayed rewards. Diss. University of Cambridge, 1989. Link (http://www.cs.rhul.ac.uk/~chrisw/thesis.html)
 
-Link (http://www.cs.rhul.ac.uk/~chrisw/thesis.html)
+MountainCar Problem Space. Link (https://en.wikipedia.org/wiki/Mountain_Car)
 
+State space
+0 position [-1.2, 0.6] goal 0.5
+1 velocity [-0.07, 0.07]
+
+Action space
+0	push left
+1	no push
+2	push right
 """
 class QLearningAgent(object):
-    seed = 0
-    accumulated_reward = 0
-    no_of_velocity_bins = 2
-    no_of_actions = 3
+    seed = None
+    actions = [0, 1, 2]
     epsilon = 0.01
     q = None
     previous_observation = None
     previous_action = None
+    discount_factor = 0.1
 
     def __init__(self, env, seed):
         self.env = env
-        self.action_n = env.action_space.n
-        self.observation_space = env.observation_space
+        self.seed = seed
         np.random.seed(seed)
-        init_q = np.zeros(self.no_of_velocity_bins * self.no_of_actions, dtype=np.float)
-        self.q = init_q.reshape(self.no_of_velocity_bins, self.no_of_actions)
+        self.observation_space = env.observation_space
+        # 3 actions and a positive/negative veloicty
+        self.q = np.zeros(env.action_space.n * 2).reshape(3,2)
 
-    """
-    0 position -1.2:0.6
-    1 velocity -0.07:0.07
-    """
-    def bin_index(self, value):
-        if value > 0:
-            return 0
-        else:
-            return 1
-
-    def act(self, observation, reward, step, done):
-        print("observation {}",observation[0])
-        # print("{}", )
-        v_bin = self.bin_index(observation[1])
-        print("v_bin {} p_bin {}", v_bin)
-        action_q = self.q[v_bin]
-        print(action_q)
-        action = action_q.argmax(axis=0) if np.random.random() > self.epsilon else self.env.action_space.sample()
-        # action = np.amax(q[v_bin, p_bin]) if np.random.random() > self.epsilon else self.env.action_space.sample()
-
-        # was there a  previous action to update the q
+    def act(self, observation, reward, step):
+        print("step {} reward {} observation {}".format(step, reward, observation))
+        action = self.q.argmax(axis=0) + 1 if np.random.random() > self.epsilon else self.env.action_space.sample()
+        if hasattr(action, "__iter__"):
+            action = action[0]
         if self.previous_action is not None:
-            learning_rate = 200 / (300 / step)
+            learning_rate = 200 / (300 + step)
             current_q = self.q
-            next_best_q = np.argmax(self.q[observation.item()])
-            updated_q = (1 - learning_rate) * current_q + learning_rate * (reward + discount_factor * next_best_q)
-        # double learningRate = (200.0 / (300.0 + timestep));
-        # double currentQ = QValues[state.center + offset][action.getValue() + offset];
-        # double QForNextState = getBestQForState(nextState);
-        # double updatedQValue =
-        #         (1.0 - learningRate) * currentQ + learningRate * (reward + discountFactor * QForNextState);
-        # QValues[state.center + offset][action.getValue() + offset] = updatedQValue;
+            next_best_q = 1 # np.argmax(self.q[observation.item()])
+            updated_q = (1 - learning_rate) * current_q + learning_rate * (reward + self.discount_factor * next_best_q)
 
-        previous_observation = observation # for the next step
-        previous_action = action # not in the observation?
-        
+        self.previous_observation = observation # for the next step
+        self.previous_action = action # not in the observation?
         return action
 
 if __name__ == '__main__':
@@ -82,15 +65,15 @@ if __name__ == '__main__':
     ob = env.reset()
     step = 0
     while True:
-    	action = agent.act(ob, reward, step, done)
-    	ob, reward, done, _ = env.step(action)
-	step = step + 1
-	total_reward += reward
-    	if done:
-	    if ob[0] >= 0.5:
-	    	print(("Solved in {} steps with a total reward of {}").format(step, total_reward))
-	    else:
-	    	print(("Not solved in {} (max) steps with a total reward of {}").format(step, total_reward))
-
+        action = agent.act(ob, reward, step)
+        print("Action {}".format(action))
+        ob, reward, done, _ = env.step(action)
+        step = step + 1
+        total_reward += reward
+        if done:
+            if ob[0] >= 0.5:
+                print(("Solved in {} steps with a total reward of {}").format(step, total_reward))
+            else:
+                print(("Not solved in {} (max) steps with a total reward of {}").format(step, total_reward))
             break
     env.close()
